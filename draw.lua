93d0b33b0c6cfa4021bcf81105104b590da99a39
ashifolfi <anticream39@yahoo.com>
2022-12-29 02:15:10 -0500

    implement file opening + JSON opening

--- draw.lua
+++ draw.lua
@@ -1,19 +1,40 @@
 local imgui = require "cimgui"
 local ffi = require "ffi"
 
-local io = imgui.GetIO()
+local imio = imgui.GetIO()
 
 local propview = require "propview"
+local fbg = require "libs.filebrowser"(imgui)
+local json = require "libs.json"
 
 local show = ffi.new("bool[1]", true)
 
 local files = {}
 local curfile = 0
 
+local to_open = nil
+
+local opendialog = fbg.FileBrowser(nil,{key="loader",pattern="",
+curr_dir=love.filesystem.getSourceBaseDirectory()},function(fname) 
+    print("load ",fname)
+    to_open = fname
+end)
+
 return function()
+    local open_file = false
     -- fullscreen window of tabs for property files
+    if to_open then
+        local file = io.open(to_open, "r")
+        local fd = file:read("*all")
+        file:close()
+        table.insert(files, #files+1, {
+            name = to_open,
+            data = json.decode(fd)
+        })
+        to_open = nil
+    end
     
-    imgui.SetNextWindowSize(imgui.ImVec2_Float(io.DisplaySize.x, io.DisplaySize.y))
+    imgui.SetNextWindowSize(imgui.ImVec2_Float(imio.DisplaySize.x, imio.DisplaySize.y))
     imgui.SetNextWindowPos(imgui.ImVec2_Float(0, 0))
     if (imgui.Begin("propedit", show, 
     imgui.WindowFlags("MenuBar", "NoResize", "NoMove", "NoTitleBar", "NoSavedSettings"))) then
@@ -24,6 +45,7 @@ return function()
                 end
                 imgui.Separator()
                 if imgui.MenuItem_Bool("Open") then
+                    open_file = true
                 end
                 imgui.Separator()
                 if imgui.MenuItem_Bool("Save") then
@@ -79,6 +101,10 @@ return function()
             imgui.EndTabBar()
         end
 
+        if open_file then
+            opendialog.open()
+        end
+        opendialog.draw()
     end
     imgui.End()
 end
--- propview.lua
+++ propview.lua
@@ -5,14 +5,6 @@ item_selected = ""
 
 return function(tbl)
     local function recurse(tbl)
-
-    end
-
-    if imgui.BeginTable("##tableview", 2, imgui.ImGuiTableFlags_RowBg) then
-        imgui.TableSetupColumn("Key", ImGuiTableColumnFlags_NoHide)
-        imgui.TableSetupColumn("Value", ImGuiTableColumnFlags_NoHide)
-        imgui.TableHeadersRow()
-
         for k,v in pairs(tbl) do
             local sel = false
             if item_selected == k then
@@ -24,15 +16,31 @@ return function(tbl)
                 imgui.PushStyleColor_Vec4(imgui.ImGuiCol_TableRowBg, 
                 imgui.ImVec4_Float(0.26, 0.59, 0.98, 0.4))
             end
-            if imgui.Selectable_Bool(k, sel) then
-                item_selected = k
+            if type(v) ~= "table" then
+                if imgui.Selectable_Bool(tostring(k), sel) then
+                    item_selected = k
+                end
+                imgui.TableNextColumn()
+                imgui.Selectable_Bool(tostring(v))
+            else
+                if (imgui.TreeNodeEx_Str(k, imgui.ImGuiTreeNodeFlags_SpanFullWidth)) then
+                   recurse(v)
+                   imgui.TreePop()
+                end
+                imgui.TableNextColumn()
             end
             if sel then
                 imgui.PopStyleColor()
             end
-            imgui.TableNextColumn()
-            imgui.Selectable_Bool(v)
         end
+    end
+
+    if imgui.BeginTable("##tableview", 2, imgui.ImGuiTableFlags_RowBg) then
+        imgui.TableSetupColumn("Key", ImGuiTableColumnFlags_NoHide)
+        imgui.TableSetupColumn("Value", ImGuiTableColumnFlags_NoHide)
+        imgui.TableHeadersRow()
+
+        recurse(tbl)
 
         imgui.EndTable()
     end
